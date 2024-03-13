dc7f4670835fb8acd437c08e158193c001cf2eb8
samoyla <samoyla@hotmail.com>
2024-03-13 13:35:08 +0100

    RestTemplate to retrieve a random Spring Boot quotation

---
+++ src/test/java/samoyla/RestTemplate/RestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package samoyla.RestTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
